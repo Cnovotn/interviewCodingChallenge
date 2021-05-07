9b169f45896a9691aa22450684b7b802cd15bcaf
Clayton Novotney <claytonnovotney@gmail.com>
2021-05-07 00:50:04 -0700

    added a bunch of comments and test functions/cases

--- index copy.js
+++ index copy.js
@@ -1,4 +1,25 @@
 /**
+ * B-I-N-G-O
+ *
+ * A Bingo card contain 25 squares arranged in a 5x5 grid (five columns
+ * and five rows). Each space in the grid contains a number between 1
+ * and 75. The center space is marked "FREE" and is automatically filled.
+ *
+ * As the game is played, numbers are drawn. If the player's card has
+ * that number, that space on the grid is filled.
+ *
+ * A player wins BINGO by completing a row, column, or diagonal of filled
+ * spaces.
+ *
+ * Your job is to complete the function that takes a bingo card and array
+ * of drawn numbers and return 'true' if that card has achieved a win.
+ *
+ * A bingo card will be 25 element array. With the string 'FREE' as the
+ * center element (index 12). Although developers are unscrupulous, they
+ * will pass valid data to your function.
+ */
+
+ /**
   * Clayton Novotney
   * Coding Interview for Hoyla 
   * Positon: Software Engineer
@@ -9,7 +30,7 @@
   * I definitiely overcomplicated this solution by using classes and have multiple functions that could have 
   * been a lot smaller, but for the sake of demonstrating my knowledge, I wanted to implement as many different
   * features as possible.
-*/
+  */
 
 // Gameboard class used for storing all of the bingo information and handling checks for a winning board or not.
 class GameBoard {
@@ -23,7 +44,7 @@ class GameBoard {
     }
     this.winningPieces = picked;
   }
-  
+
   // function that isn't used in this specific instance, but if we wanted to use this software for a running
   // game of bingo, we could add more pieces to the winning pieces and continue to check to see if it is a winning
   // board or not. 
@@ -71,13 +92,13 @@ class GameBoard {
   checkRows(){
     for(var i = 0; i < this.arrayBoard.length; i++) {
       let thisRow = this.arrayBoard[i];
-      if(this.checkRow(thisRow)) {
-        return true;
+      if(!this.checkRow(thisRow)) {
+        return false;
       }
     }
-    return false;
+    return true;
   }
-
+  
   // Checking all of the columns to see if it is a winning board.
   checkColumns() {
     for(var i = 0; i < this.arrayBoard.length; i++) {
@@ -85,11 +106,11 @@ class GameBoard {
       for(var j = 0; j< this.arrayBoard.length; j++) {
         thisColumn.push(this.arrayBoard[j][i]);
       }
-      if(this.checkRow(thisColumn)) {
-        return true;
+      if(!this.checkRow(thisColumn)) {
+        return false;
       }
     }
-    return false;
+    return true;
   }
 
   // Checking the top left to bottom right diagonal pieces
@@ -115,12 +136,13 @@ class GameBoard {
   }
 }
 
+
 function checkForBingo (bingoCard, drawnNumbers) {
   let game = new GameBoard(bingoCard, drawnNumbers);
   if(game.checkRows() || game.checkColumns() || game.checkLeftDiagonal() || game.checkRightDiagonal()) {
     game.printWinningBoard();
     return true;
-  }
+  }  
   console.log("Not a winning gameboard:(");
   game.printBoard();
   return false;
@@ -128,20 +150,116 @@ function checkForBingo (bingoCard, drawnNumbers) {
 
 module.exports = checkForBingo;
 
-function testFunctions() {
-  const gameboard = [8, 29, 35, 54, 65, 13, 24, 44, 48, 67, 9, 21, 'FREE', 59, 63, 7, 19, 34, 53, 61, 1, 20, 33, 46, 72];
-  const verticalNoFree = [8, 13, 9, 7, 1];
-  const verticalWithFree = [35, 44, 34, 33];
-  const horizontalNoFree = [8, 29, 35, 65, 54];
-  const horizontalWithFree = [9, 21, 59, 63];
-  const topLeftDiagonal = [8, 24, 53, 72];
-  const topRightDiagonal = [65, 48, 19, 1];
-  const loser = [1, 33, 53, 65, 29, 75];
-  const testCases = [verticalNoFree,verticalWithFree, horizontalNoFree, horizontalWithFree, topLeftDiagonal, topRightDiagonal, loser];
-  testCases.forEach(element => {
-    checkForBingo(gameboard, element);
-  });
+// here are some samples
+
+function testBingos() {
+  let verticalNoFree = [[
+      8, 29, 35, 54, 65,
+      13, 24, 44, 48, 67,
+      9, 21, 'FREE', 59, 63,
+      7, 19, 34, 53, 61,
+      1, 20, 33, 46, 72
+    ],
+    [
+      8, 13, 9, 7, 1
+    ]]
+    checkForBingo(verticalNoFree[0], verticalNoFree[1]);
 }
 
-testFunctions();
+testBingos();
+// this should return true with vertical without free
+checkForBingo(
+  [
+    8, 29, 35, 54, 65,
+    13, 24, 44, 48, 67,
+    9, 21, 'FREE', 59, 63,
+    7, 19, 34, 53, 61,
+    1, 20, 33, 46, 72
+  ],
+  [
+    8, 13, 9, 7, 1
+  ]
+);
 
+// this should return true with vertical + free
+// checkForBingo(
+//   [
+//     8, 29, 35, 54, 65,
+//     13, 24, 44, 48, 67,
+//     9, 21, 'FREE', 59, 63,
+//     7, 19, 34, 53, 61,
+//     1, 20, 33, 46, 72
+//   ],
+//   [
+//     35, 44, 34, 33
+//   ]
+// );
+
+// this should return true with horizontal without free
+// checkForBingo(
+//   [
+//     8, 29, 35, 54, 65,
+//     13, 24, 44, 48, 67,
+//     9, 21, 'FREE', 59, 63,
+//     7, 19, 34, 53, 61,
+//     1, 20, 33, 46, 72
+//   ],
+//   [
+//     8, 29, 35, 65, 54
+//   ]
+// );
+
+// // this should return true with horizontal + free
+// checkForBingo(
+//   [
+//     8, 29, 35, 54, 65,
+//     13, 24, 44, 48, 67,
+//     9, 21, 'FREE', 59, 63,
+//     7, 19, 34, 53, 61,
+//     1, 20, 33, 46, 72
+//   ],
+//   [
+//     9, 21, 59, 63
+//   ]
+// );
+
+// this should return true with top left start diagonal + free
+// checkForBingo(
+//   [
+//     8, 29, 35, 54, 65,
+//     13, 24, 44, 48, 67,
+//     9, 21, 'FREE', 59, 63,
+//     7, 19, 34, 53, 61,
+//     1, 20, 33, 46, 72
+//   ],
+//   [
+//     8, 24, 53, 72
+//   ]
+// );
+// this should return true with top right start diagonal + free
+// checkForBingo(
+//   [
+//     8, 29, 35, 54, 65,
+//     13, 24, 44, 48, 67,
+//     9, 21, 'FREE', 59, 63,
+//     7, 19, 34, 53, 61,
+//     1, 20, 33, 46, 72
+//   ],
+//   [
+//     65, 48, 19, 1
+//   ]
+// );
+
+// // this should return false
+// checkForBingo(
+//   [
+//    8, 29, 35, 54, 65,
+//    13, 24, 44, 48, 67,
+//    9, 21, 'FREE', 59, 63,
+//    7, 19, 34, 53, 61,
+//    1, 20, 33, 46, 72
+//   ],
+//   [
+//     1, 33, 53, 65, 29, 75
+//   ]
+// );
